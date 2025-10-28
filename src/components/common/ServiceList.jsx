eaa4c25ab5d0cfe80664c49a60fc54827f1c7b04
Amit-Biswas-WD <155246211+Amit-Biswas-WD@users.noreply.github.com>
2025-10-28 10:18:08 +0600

    Common ServiceList Components

---
+++ src/components/common/ServiceList.jsx
@@ -0,0 +1,20 @@
+import { FaArrowRight } from 'react-icons/fa';
+import P from '../ui/P';
+
+const ServiceList = ({ items = [] }) => {
+  return (
+    <div className="max-w-[586px] rounded-2xl bg-[#F1F9F6] px-[35px] py-5">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className={`flex items-center gap-3 ${index !== 0 ? 'my-4' : ''}`}
+        >
+          <FaArrowRight className="text-[#284338]" />
+          <P p={item} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ServiceList;
