68132fdfa8bd9bfd28e1fcf94af79dc2afd3e51f
Hasan Talukder <mahadihasansiam2@gmail.com>
2025-11-03 12:51:19 +0600

    licensense

--- src/router/dashboardRoutes.jsx
+++ src/router/dashboardRoutes.jsx
@@ -3,6 +3,7 @@ import TeacherView from '../pages/admin/freelancer/TeacherView';
 import VideosView from '../pages/admin/freelancer/VideosView';
 import DocsView from '../pages/admin/students/DocsView';
 import StudentView from '../pages/admin/students/StudentView';
+import LicenseeSuperAdminDashboard from '../pages/admin/super/sections/LicenseManagementSuperAdmin';
 import SuperAdminView from '../pages/admin/super/SuperAdminView';
 
 export const dashboardRoutes = [
@@ -10,7 +11,7 @@ export const dashboardRoutes = [
     roles: ['admin'],
     routes: [
       { path: 'super-admin', element: <SuperAdminView /> },
-      { path: 'gestione-licenze', element: <h1>Gestione Licenze</h1> },
+      { path: 'gestione-licenze', element: <LicenseeSuperAdminDashboard /> },
       { path: 'impostazioni', element: <h1>Impostazioni</h1> },
       { path: 'ticket', element: <h1>Ticket</h1> },
       { path: 'feedback', element: <h1>Feedback</h1> },
